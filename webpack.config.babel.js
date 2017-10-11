357c7626a419d95346f9d6116e25159aac8e32dc
AlexL <alexander.loehn@elbformat.de>
2017-10-11 14:30:22 +0200

    Fixed build issues

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,43 +4,43 @@ import path from 'path'
 const { NODE_ENV } = process.env
 
 const plugins = [
-  new webpack.optimize.OccurrenceOrderPlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
-  })
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    })
 ]
 
-const filename = `redux-undo${NODE_ENV === 'production' ? '.min' : ''}.js`
+const filename = `index.js`
 
 NODE_ENV === 'production' && plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      pure_getters: true,
-      unsafe: true,
-      unsafe_comps: true,
-      screw_ie8: true,
-      warnings: false
-    }
-  })
+    new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+            pure_getters: true,
+            unsafe: true,
+            unsafe_comps: true,
+            screw_ie8: true,
+            warnings: false
+        }
+    })
 )
 
 export default {
-  module: {
-    rules: [
-      { test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/ }
-    ]
-  },
+    module: {
+        rules: [
+            { test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/ }
+        ]
+    },
 
-  entry: [
-    './src/index'
-  ],
+    entry: [
+        './src/index'
+    ],
 
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename,
-    library: 'ReduxUndo',
-    libraryTarget: 'umd'
-  },
+    output: {
+        path: __dirname,
+        filename,
+        library: 'ReduxUndo',
+        libraryTarget: 'umd'
+    },
 
-  plugins
+    plugins
 }
